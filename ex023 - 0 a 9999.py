c45b4d8a9a7747b87d400dced82a5b6048e5d317
caiowestphalen <caiowestphalen@hotmail.com>
2022-04-22 11:28:15 -0300

    Add files via upload

---
+++ ex023 - 0 a 9999.py
@@ -0,0 +1,16 @@
+# Programa que separa MILHAR, CENTENA, DEZENA e UNIDADE
+
+num = int(input('Digite um número: '))
+u = num // 1 % 10
+d = num // 10 % 10
+c = num // 100 % 10
+m = num // 1000 % 10
+print('Analisando o número {}'.format(num))
+print('Unidades: {}'.format(u))
+print('Dezena: {}'.format(d))
+print('Centena: {}'.format(c))
+print('Milhar: {}'.format(m))
+print('='*20)
+
+
+
